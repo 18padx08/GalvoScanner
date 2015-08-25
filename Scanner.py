b236b9546dc28c1f0387e21cc404c29ed08f0ba7
patrick <padx@sunrise.ch>
2015-08-25 09:25:12 +0200

    apd init like in example

--- Scanner.py
+++ Scanner.py
@@ -104,10 +104,18 @@ class Scanner:
 		self.autocorrection = False
 		self.quadSize = 3
 		self.noCheckForMax = True
+		#accept any device
 		TDC_init(-1)
+		#enable all channels
+		TDC_enableChannels(0xff)
+		#enable hbt
+		TDC_enableHbt(True)
+		#enable start stop
+		TDC_enableStartStop(True)
 		#exposure time in ms
 		self.exposureTime = 1
 		TDC_setExposureTime(self.exposureTime)
+		TDC_clearAllHistograms()
 		#the calibration values, read them from the config file
 		import json
 		import os.path
